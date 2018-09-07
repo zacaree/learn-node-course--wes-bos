43c40f4d121c96bede7e8f6a09cce783b40fdeb6
Zac <zacaree@gmail.com>
2018-09-07 15:55:09 -0500

    EOD Checkpoint

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log('hey!!!!🔥 Yo');
+  // res.send('Hey! It works!');
+  // const zac = { name: 'Zac', age: 31, cool: true };
+  // res.json(zac);
+  // res.send(req.query.name);
+  res.send(req.query);
 });
 
 module.exports = router;
