fdac340e0280761435299dc523850e48bc508c3a
Zac <zacaree@gmail.com>
2018-09-11 12:25:11 -0500

    Just started video #11

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose');
+
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 };
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('./../controllers/storeController')
 
-// Do work here
+
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
+
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
