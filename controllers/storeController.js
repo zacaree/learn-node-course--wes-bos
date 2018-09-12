4478c547ff2cedb89109e33131fc1aca2130a13c
zacaree <zacharem@gmail.com>
2018-09-12 16:24:05 -0500

    Ready to start video 14

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
   console.log(req.name);
@@ -9,6 +10,14 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  // res.json(req.body);
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores: stores });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require('./../controllers/storeController')
+const storeController = require('./../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 
 router.get('/reverse/:name', (req, res) => {
