96a20f6bc158c202a86f7bbe1a65b32ca7a87600
Zac <zacaree@gmail.com>
2018-09-24 16:21:15 -0500

    Wire up the login button

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 router.post('/register', 
