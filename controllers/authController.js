4e997778bf3e229de2af1b29329aa12ccd3e89e7
Zac <zacaree@gmail.com>
2018-09-25 10:06:01 -0500

    Setup functionality that allows user to change their password.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -55,3 +56,33 @@ exports.reset = async (req, res) => {
   // If there is a user, show the reset password form
   res.render('reset', { title: 'Reset your password' });
 }
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']) {
+    next(); // Keep it going
+    return;
+  }
+  req.flash('error', 'Passwords do not match!');
+  res.redirect('back');
+};
+
+exports.update = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+
+  const setPassword = promisify(user.setPassword, user);
+  await setPassword(req.body.password);
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpires = undefined;
+  const updatedUser = await user.save();
+  await req.login(updatedUser);
+  req.flash('success', '💃 Nice! Your password has been reset! You are now logged in!');
+  res.redirect('/');
+};
--- routes/index.js
+++ routes/index.js
@@ -49,7 +49,10 @@ router.get('/account',
 router.post('/account', catchErrors(userController.updateAccount));
 router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
-
+router.post('/account/reset/:token',
+  authController.confirmedPasswords,
+  catchErrors(authController.update)
+);
 
 
 // Just for fun - not for the app
