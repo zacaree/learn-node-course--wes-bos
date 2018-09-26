b72592ab574a9b85ecc1ad446be97849a7343fb0
zacaree <zacharem@gmail.com>
2018-09-26 13:39:59 -0500

    So pumped! Just sent my very first email without an email client!

---
+++ handlers/mail.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const juice = require('juice');
+const htmlToText = require('html-to-text');
+const promisify = require('es6-promisify');
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+transport.sendMail({
+  from: 'Zac Remboldt <ZacRemboldt@gmail.com>',
+  to: 'ralph@example.com',
+  subject: 'My first Node mail!',
+  html: 'Hey check it out man! <strong>Bold text</strong>.',
+  text: 'Hey check it out man! **Bold text**.'
+});
--- start.js
+++ start.js
@@ -29,3 +29,6 @@ app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
+// Temp send email
+require('./handlers/mail');
