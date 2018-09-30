62f41220fd4cc230290e8c992474c108660a9429
zacaree <zacharem@gmail.com>
2018-09-29 21:30:16 -0500

    Finished up search functionality!

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+import axios from 'axios';
+import dompurify from 'dompurify'; // This library is designed to protect us from XSS attacks when setting innerHTML
 
 function searchResultsHTML(stores) {
   return stores.map(store => {
@@ -11,11 +12,13 @@ function searchResultsHTML(stores) {
 }
 
 function typeAhead(search) {
+  // If somehow there is no search element, stop the function now
   if (!search) return;
 
   const searchInput = search.querySelector('input[name="search"]');
   const searchResults = search.querySelector('.search__results');
 
+  // .on is identical to an event listener. It's available to us because of Bling.js
   searchInput.on('input', function() {
     // If there is not value, quit it! Hide the results.
     if (!this.value) {
@@ -24,13 +27,17 @@ function typeAhead(search) {
     }
     // Show the search results
     searchResults.style.display = 'block';
-    searchResults.innerHTML = '';
+
+    // Axios is just a fetch library with perks
     axios
       .get(`/api/search?q=${this.value}`)
       .then(res => {
         if (res.data.length) {
-          searchResults.innerHTML = searchResultsHTML(res.data);
+          searchResults.innerHTML = dompurify.sanitize(searchResultsHTML(res.data));
+          return;
         }
+        // Tell the user that nothing came back from the DB
+        searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No results for "${this.value}" found.</div>`);
       })
       .catch(err => {
         console.error(err);
@@ -44,7 +51,26 @@ function typeAhead(search) {
       return; // Ignore
     }
     const activeClass = 'search__result--active';
-  })
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+    let next;
+    if (e.keyCode === 40 && current) { // If down is pressed and there is already an active item in the list then make the next item active or the first item.
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) { // If none are active yet then "next" becomes the first item in the list, making it active.
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keycode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) { // If enter is pressed and the current element has a link
+      window.location = current.href;
+      return;
+    }
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
+  });
 }
 
 export default typeAhead;
