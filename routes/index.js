a67d0493def1b1ba50ae2bfe6f4ff58b91da979d
zacaree <zacharem@gmail.com>
2018-10-02 06:45:19 -0500

    Setting up liked stores

--- routes/index.js
+++ routes/index.js
@@ -54,7 +54,7 @@ router.get('/map', storeController.mapPage);
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
-
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 
 
