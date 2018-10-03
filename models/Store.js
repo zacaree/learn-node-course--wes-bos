d03f057990fd49994e659d26e1ad9073f42d1c8e
zacaree <zacharem@gmail.com>
2018-10-03 10:18:21 -0500

    Created DB query to create a top 10 list of stores based on reviews.

--- models/Store.js
+++ models/Store.js
@@ -77,7 +77,37 @@ storeSchema.statics.getTagsList = function() {
     { $group: { _id: '$tags', count: { $sum: 1 } } },
     { $sort: { count: -1 } }
   ]);
-}
+};
+
+// This will return a promise which is why the function calling it in storeController.js is set to await
+storeSchema.statics.getTopStores = function() {
+  // aggregate is a query function. It's much like .find
+  return this.aggregate([
+    // Lookup stores and populate their reviews
+    // This takes our stores document and our reviews document and merges them together
+    { $lookup: { 
+      from: 'reviews', 
+      localField: '_id', 
+      foreignField: 'store', 
+      as: 'reviews' 
+    }},
+    // Filter keeping only stores that have two or more reviews
+    // 0 is the first, 1 is the second. So where a second review exists...
+    { $match: { 'reviews.1': { $exists: true } } },
+    // Find the average of all reviews on a store
+    { $project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      slug: '$$ROOT.slug',
+      reviews: '$$ROOT.reviews',
+      averageRating: { $avg: '$reviews.rating' }
+    }},
+    // Sort list of stores by their average rating, highest first
+    { $sort: { averageRating: -1 } },
+    // Limit the list to a max of 10 stores
+    { $limit: 10 }
+  ]);
+};
 
 // This creates a virtual relationship between Store and Review on the DB.
 // Essentially saying, find reviews where the store's _id property === the review's store property
--- routes/index.js
+++ routes/index.js
@@ -56,6 +56,7 @@ router.post('/reviews/:id',
   authController.isLoggedIn, 
   catchErrors(reviewController.addReview)
 );
+router.get('/top', catchErrors(storeController.getTopStores));
 
 /* 
   API
